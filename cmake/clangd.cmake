4752ce4e40021072d49f507439ad680f57425a9a
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-07-03 06:39:34 +0700

    fixing clangd

--- cmake/clangd.cmake
+++ cmake/clangd.cmake
@@ -13,16 +13,3 @@ if(CMAKE_EXPORT_COMPILE_COMMANDS AND NOT CMAKE_GENERATOR MATCHES "Visual Studio|
     COMMENT "Mirroring compile_commands.json to project root for clangd"
     VERBATIM)
 endif()
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_SIMULATE_ID STREQUAL "MSVC")
-  add_custom_target(
-    fix_clangd_compile_commands ALL
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different 
-      "${CMAKE_SOURCE_DIR}/compile_commands.json"
-      "${CMAKE_SOURCE_DIR}/compile_commands.json.bak"
-    COMMAND powershell -Command 
-      "(Get-Content '${CMAKE_SOURCE_DIR}/compile_commands.json' -Raw) -replace '/imsvc', '-I' | Out-File -Encoding ASCII '${CMAKE_SOURCE_DIR}/compile_commands.json'"
-    DEPENDS mirror_compile_commands
-    COMMENT "Converting clang-cl compile commands for clangd compatibility"
-    VERBATIM)
-endif()
